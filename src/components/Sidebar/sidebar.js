38e56f341262c037c2c2f8fc89b503801a2df406
antb4 <ant5692@gmail.com>
2023-10-21 14:23:49 -0700

    added sidebar icons and names

--- src/components/Sidebar/sidebar.js
+++ src/components/Sidebar/sidebar.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import './sidebar.css'
 import headshot from '../../assets/self-photo.png';
+import { SidebarButton } from '../sidebarButtons/sidebarButton';
+import { MdFavorite } from "react-icons/md";
+import { FaGripfire, FaPlay } from "react-icons/fa";
+import { FaSignOutAlt } from "react-icons/fa";
+import { IoLibrary } from "react-icons/io5";
+import { MdSpaceDashboard } from "react-icons/md";
 
 export default function Sidebar() {
   return (
@@ -8,8 +14,14 @@ export default function Sidebar() {
       <img className="profile-img" alt="profile headshot" src={headshot}></img>
 
       <section>
-        <SidebarButton />
+        <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />}/>
+        <SidebarButton title="Trending" to="/trending" icon={<FaGripfire />} />
+        <SidebarButton title="Player" to="/player" icon={<FaPlay />} />
+        <SidebarButton title="Favorites" to="/favorites" icon={<MdFavorite />} />
+        <SidebarButton title="Library" to="/" icon={<IoLibrary />} />
       </section>
+      <SidebarButton title="Sign Out" to="" icon={<FaSignOutAlt />} />
+
     </div>
   )
 }
--- src/components/sidebarButtons/sidebarButton.js
+++ src/components/sidebarButtons/sidebarButton.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import './sidebarButton.css'
+import { Link } from 'react-router-dom'
 
-export const SidebarButton = () => {
+export const SidebarButton = (props) => {
+
   return (
-    <div>sidebarButton</div>
+    <>
+    <Link to={props.to}>
+      <section>
+        {props.icon}
+        <p>{props.title}</p>
+      </section>
+    </Link>
+    </>
   )
 }
