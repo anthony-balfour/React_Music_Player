9402e466c378df8000f16a63886e08271eff5509
antb4 <ant5692@gmail.com>
2023-10-23 13:18:12 -0700

    check if hash is an item in local storage

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -15,15 +15,29 @@ function Home() {
   const [token, setToken] = useState('');
 
   useEffect(() => {
+
+    let token = window.localStorage.getItem("access-token");
+    const hash = window.location.hash;
+
+    // clearing the hash since it's saved in local
+    window.location.hash="";
+    if (!token && hash) {
+      token = hash.split("&")[0].split("=")[1];
+      window.localStorage.setItem("access-token", token);
+      setToken(token);
+    } else {
+      setToken(token);
+    }
     // grabs the hash in the window's locationm which starts with "#access_token="
     // has the token type and the expires in, all separated by "&"
     // split will return an array with the access token as the first element
-    const hash = window.location.hash;
-    const token = hash.split("&")[0].split("=")[1];
-    window.localStorage.setItem("access-token", token);
-    setToken(token);
-  })
+
+  }, []);
+
   return (
+    // if the access token is not in state, then the login page is displayed,
+    // else all other routes and the home library page is rendered
+    !token ? <Login /> : (
     <Router>
       <main className="main">
         <Sidebar />
@@ -38,6 +52,7 @@ function Home() {
         </Routes>
       </main>
     </Router>
+    )
   )
 }
 
