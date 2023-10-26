d1ff3b3f3c01e3cd98ca2899f4d7ddfc93139aab
antb4 <ant5692@gmail.com>
2023-10-25 22:15:03 -0700

    playlists api call

--- src/pages/Library/library.js
+++ src/pages/Library/library.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import apiClient from '../../spotify'
+import './library.css';
 
 export default function Library() {
+
+  const [playlists, setPlaylists] = useState(null);
+
+  useEffect(() => {
+    apiClient.get("me/playlists")
+    .then(response => {
+      setPlaylists(response.data.items);
+    })
+  }, []);
   return (
-    <div className='page-container'>Library</div>
+    <div className='page-container'>
+      <main className='library-body'>
+        {playlists?.map((playlist) => {
+          console.log(playlist);
+          return <div>{playlist.name}</div>
+        })}
+      </main>
+    </div>
   )
 }
