f9951c90c6092b1a4ba766e0b4bb77e32e4bd246
antb4 <ant5692@gmail.com>
2023-10-30 15:02:05 -0700

    added playbutton and useNavigate to player

--- src/pages/Library/library.js
+++ src/pages/Library/library.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import apiClient from '../../spotify'
 import spotify from '../../assets/spotify.jpg';
+// can use for styling react icons
 import { IconContext } from "react-icons";
 import {AiFillPlayCircle } from "react-icons/ai";
 import './library.css';
+
+import {useNavigate} from 'react-router-dom';
 
 export default function Library() {
 
@@ -15,15 +18,21 @@ export default function Library() {
       setPlaylists(response.data.items);
     })
   }, []);
+
+  const navigate = useNavigate();
+
+  const playPlaylist = (id) => {
+    navigate('/player', {state: {id: id}});
+  }
   return (
     <div className='page-container'>
       <main className='library-body'>
         {playlists?.map((playlist) => {
 
           const image = playlist.images[0] ? playlist.images[0].url : spotify;
-          console.log(playlist);
-          return <div className="playlist-card">
-            <img src={image} className="playlist-image" alt="Playlist Art" />
+          return <div className="playlist-card" key={playlist.id} onClick={() => playPlaylist(playlist.id)}>
+            <img src={image}
+            className="playlist-image" alt="Playlist Art" />
             <p className="playlist-title">{playlist.name}</p>
             <p className="playlist-subtitle">{playlist.tracks.total} Songs</p>
             <div className="playlist-fade">
