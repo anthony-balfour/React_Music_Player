f3b950eafcc86db33970fd92b7a3a3a37aecc04a
antb4 <ant5692@gmail.com>
2023-11-14 21:24:50 -0800

    track styling

--- src/components/Queue/queue.js
+++ src/components/Queue/queue.js
@@ -4,15 +4,16 @@ import React from 'react'
 import './queue.css';
 
 export const Queue = ({ tracks, setCurrentIndex}) => {
+  console.log(tracks);
   return (
     <div className="queue-container flex">
       <div className="queue flex">
         <p className="upNext">Up Next</p>
         <div className="queue-list">
           {
-          tracks.map(track => {
+          tracks?.map(track => {
             <div>
-              <p></p>
+              <p className="track-name">{track?.track.name}</p>
               <p></p>
             </div>
           })
