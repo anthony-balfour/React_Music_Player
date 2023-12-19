e944315af21f56e23b24f0bfa38bfeb8da405b8c
antb4 <ant5692@gmail.com>
2023-12-18 19:54:01 -0800

    added WidgetEntries for each WidgetCard

--- src/components/Widgets/WidgetCard/widgetCard.js
+++ src/components/Widgets/WidgetCard/widgetCard.js
@@ -5,10 +5,44 @@
 import React from 'react'
 import './widgetCard.css';
 
-export default function WidgetCard() {
+/**
+ *
+ * @param {String} title - Title of the widget card, either Featured artists,
+ * new releases, or related artists
+ * @returns
+ */
+export default function WidgetCard({title, similarArtists, featured, newRelease}) {
   return (
-    <section>
-      WidgetCard
-      </section>
+
+    <section className="widget-card-body">
+      <p className="widget-title">{title}</p>
+      {
+
+        similarArtists ? similarArtists.map(artist => {
+          <WidgetEntry
+            title={artist.name}
+            subtitle={artist.followers.total}
+            // third image is the smallest image of the 3 given
+            image={artist.images[2].url}
+          />
+        }) : featured ? featured.map(playlist => {
+          <WidgetEntry
+            title={playlist.name}
+            subtitle={playlist.tracks.total}
+            // third image is the smallest image of the 3 given
+            image={playlist.images[2].url}
+          />
+        }) : newRelease ? newRelease.map(album => {
+          <WidgetEntry
+            title={album.name}
+            subtitle={album.tracks.total}
+            // third image is the smallest image of the 3 given
+            image={album.images[2].url}
+          />
+        })
+
+      }
+    </section>
   )
 }
+
